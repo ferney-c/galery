56384d9750c7effb1ece742e3b34721a85426b9b
ferney-c <ferney.caicedo@correounivalle.edu.co>
2024-11-17 22:22:25 -0500

    .

--- src/app/interfaces/interface.ts
+++ src/app/interfaces/interface.ts
@@ -2,3 +2,76 @@ export interface UserPhoto {
   filepath: string;
   webviewPath?: string;
 }
+
+export interface QuoteResponse {
+  success: number;
+  message: string;
+  quotes: Quote[];
+}
+
+export interface Quote {
+  id: number;
+  client_id: number | null;
+  barber_id: number;
+  assigned: string;
+  slots: number;
+  status: string;
+  created_at: Date;
+  updated_at: Date;
+  total_price: number;
+  client: Client | null;
+  barber: Barber | null;
+  services: Service[] | null;
+}
+
+export interface Client {
+  id: number;
+  user_id: number;
+  role_id: number;
+  barbershop_id: number | null;
+  starts: number | null;
+  first_start: string | null;
+  last_start: string | null;
+  status: string;
+  created_at: Date;
+  updated_at: Date;
+  user: User;
+}
+
+export interface Barber {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  photo: string | null;
+  nickname: string | null;
+  birth: null | string;
+  email_verified_at: Date | null;
+  created_at: Date;
+  updated_at: Date;
+  user: User | null;
+}
+
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  photo: string | null;
+  nickname: string | null;
+  birth: Date | null;
+  email_verified_at: Date | null;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export interface Service {
+  id: number;
+  barbershop_id: number;
+  name: string;
+  description: string | null;
+  duration: number;
+  price: number;
+  created_at: Date;
+  updated_at: Date;
+}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,16 +1,83 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardSubtitle, IonCardHeader, IonCardTitle, IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { informationCircleOutline } from 'ionicons/icons';
+import { Quote, QuoteResponse } from '../interfaces/interface';
+import * as moment from 'moment';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [FormsModule, CommonModule, IonIcon, IonCardTitle, IonCardHeader, IonCardSubtitle, IonCard, IonHeader, IonToolbar, IonTitle, IonContent]
 })
 export class Tab2Page {
 
-  constructor() {}
+  quoteResponse!: QuoteResponse;
+  private citasSubject = new BehaviorSubject<Quote[]>([]);
+  citas$ = this.citasSubject.asObservable();
 
+  constructor() {
+    addIcons({informationCircleOutline});
+    this.loadQuoteResponse();
+  }
+
+  async loadQuoteResponse() {
+    try {
+      const response = await fetch('assets/citas.json'); // Asegúrate de que la ruta sea correcta
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      this.quoteResponse = await response.json();
+      this.addCitas(this.quoteResponse.quotes);
+      console.log(this.quoteResponse);
+    } catch (error) {
+      console.error('There was a problem with the fetch operation:', error);
+    }
+  }
+
+  addCitas(nuevasCitas: Quote[]): boolean {
+    const citasActuales = this.citasSubject.getValue();
+    
+    const citasNoRepetidas = nuevasCitas.filter(nuevaCita => 
+      !citasActuales.some(cita => cita.id === nuevaCita.id)
+    );
+  
+    if (citasNoRepetidas.length > 0) {
+      this.citasSubject.next([...citasNoRepetidas, ...citasActuales]);
+      return true;
+    }
+    
+    return false;
+  }
+
+  fecha(fecha: string): string {
+    moment.locale('es');
+    return moment(fecha).format('l[ a las ]h:mm a');
+  }
+
+  totalServicios(cita: Quote): number {
+    return cita?.services?.reduce((acc, service) => acc + service.price, 0) || 0;
+  }
+
+  cantidadServicios(cita: Quote): number {
+    return cita?.services?.length || 0;
+  }
+
+  getColor(status: string): any {
+    switch (status) {
+      case 'RESERVADA':
+        return 'blue';
+      case 'ACEPTADA':
+        return 'blue';
+      case 'CANCELADA':
+        return 'red';
+      case 'TERMINADA':
+        return 'green';
+    }
+  }
 }
